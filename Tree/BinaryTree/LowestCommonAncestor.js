0068c0210038f42f819e6d7b9c924d85c2cafdeb
Preethi <writetopreethir@gmail.com>
2024-04-25 16:22:07 -0700

    lca of a binary tree

--- Tree/BinaryTree/LowestCommonAncestor.js
+++ Tree/BinaryTree/LowestCommonAncestor.js
@@ -12,19 +12,28 @@ node.left.left = new TreeNode(6);
 node.left.right = new TreeNode(2);
 node.left.right.left = new TreeNode(7);
 node.left.right.right = new TreeNode(4);
-node.rigth = new TreeNode(1);
+node.right = new TreeNode(1);
 node.right.left = new TreeNode(0);
-node.rigth.right = new TreeNode(8);
-
-console.log(node);
-
-
-
+node.right.right = new TreeNode(8);
 
 
 
 
 function lowestCommonAncestor(root, p, q) {
   
+  function dfs(node) {
+    console.log('node', node);
+        if(node === null)  return null;
+        if(node === p || node === q) return node;
+        const left = dfs(node.left);
+        const right = dfs(node.right);
 
+        if(left && right) return node;
+        if(!left) return right;
+        if(!right) return left;
+    }
+
+  return dfs(root);
 }
+
+console.log(lowestCommonAncestor(node, 5, 1));
